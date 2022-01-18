c94ace3f56dabc297bece40e0ce215f656246061
pao1793 <97813555+pao1793@users.noreply.github.com>
2022-01-18 17:14:38 +0900

    Update bluejelly.js

--- bluejelly.js
+++ bluejelly.js
@@ -67,7 +67,7 @@ BlueJelly.prototype.scan = function(uuid){
 BlueJelly.prototype.requestDevice = function(uuid) {
   console.log('Execute : requestDevice');
   return navigator.bluetooth.requestDevice({
-      filters: [{services: ['dd5f7232-1560-4792-953d-0b2015f15340']},{name: 'ESP32'}],
+      filters: [{name: 'ESP32-2'},{name: 'ESP32'}],
       optionalServices: [this.hashUUID[uuid].serviceUUID]})
   .then(device => {
     this.bluetoothDevice = device;
